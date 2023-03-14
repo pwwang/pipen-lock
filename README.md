3901daab73c3c6be65f38949a47b36ecedb11360
pwwang <pwwang@pwwang.com>
2023-03-13 22:38:55 -0700

    🎉 Init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # pipen-lock
+
 Process lock for pipen to prevent multiple runs at the same time
+
+## Installation
+
+```bash
+pip install -U pipen-lock
+```
+
+## Enable/Disable
+
+The plugin is enabled by default. To disable it, either uninstall it or:
+
+```python
+from pipen import Proc, Pipen
+
+# process definition
+
+class MyPipeline(Pipen):
+    plugins = ["no:lock"]
+
+```
---
+++ pipen_lock.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from pipen import plugin
+from pipen.utils import get_logger
+from filelock import FileLock, Timeout
+
+if TYPE_CHECKING:  # pragma: no cover
+    from pipen import Proc
+
+__version__ = "0.0.0"
+
+logger = get_logger("lock", "info")
+
+
+class PipenLockPlugin:
+    version = __version__
+    name = "lock"
+
+    def __init__(self):
+        self.lock = None
+
+    @plugin.impl
+    async def on_proc_start(self, proc: Proc):
+        self.lock = FileLock(proc.workdir / "proc.lock")
+        try:
+            self.lock.acquire(blocking=False)
+        except Timeout:
+            proc.log(
+                "warning",
+                "Process locked, likely handled by another pipeline instance",
+                logger=logger,
+            )
+        else:
+            self.lock.release()
+
+        self.lock.acquire()
+
+    @plugin.impl
+    async def on_proc_done(self, proc: Proc, succeeded: bool):
+        self.lock.release(True)
