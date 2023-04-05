ee32a663d2f2c8953cc15348aacecabe738fda17
pwwang <pwwang@pwwang.com>
2023-04-05 16:32:39 -0700

    🔖 0.1.1

--- pipen_lock.py
+++ pipen_lock.py
@@ -8,7 +8,7 @@ from filelock import FileLock, Timeout
 if TYPE_CHECKING:  # pragma: no cover
     from pipen import Proc
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 logger = get_logger("lock", "info")
 
