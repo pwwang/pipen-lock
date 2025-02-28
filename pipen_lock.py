1f5405939c1aebecc0ca3275a8c1d6ce665032a4
pwwang <pwwang@pwwang.com>
2025-02-28 01:14:21 -0700

    0.9.0a1

--- pipen_lock.py
+++ pipen_lock.py
@@ -10,7 +10,7 @@ from filelock import FileLock, SoftFileLock, Timeout
 if TYPE_CHECKING:  # pragma: no cover
     from pipen import Pipen, Proc
 
-__version__ = "0.8.1"
+__version__ = "0.9.0a1"
 
 logger = get_logger("lock", "info")
 
